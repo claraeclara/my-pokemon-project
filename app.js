aa054b4e8033c02786de25e3442f1195287a7062
Clara Vasconcelos <claraeclara@gmail.com>
2021-10-06 16:56:23 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+let count = 12
+
+
+function render(){
+    let container = document.getElementById('pokemon')
+    container.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${count}.svg">`    
+}
+
+let prev = document.getElementById('prev')
+let next = document.getElementById('next')
+
+prev.onclick = funcion(){
+    if (count > 1) {
+        count= count -1
+        render()
+    }
+}
+
+next.onclick = funcion(){
+    if (count < 650){
+        count= count + 1
+        render()
+    }
+}
+
+
+render()
